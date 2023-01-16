e1c064ff19ee630a8b6ef6668f09b29d8052923f
Steyer, Nick <nick.steyer@stud.tu-darmstadt.de>
2023-01-16 19:45:10 +0200

    Use version from OpenAI packing does not yet work

--- src/Main.java
+++ src/Main.java
@@ -1,96 +1,119 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Enumeration;
 import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
-import java.util.zip.ZipException;
+import java.util.jar.JarInputStream;
+import java.util.jar.JarOutputStream;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        JarFile jarFile = new JarFile("h08-ab12cdef-Max-Mustermann-submission.jar");
-        Enumeration en = jarFile.entries();
-        while (en.hasMoreElements()) {
-            String ent = proc(en.nextElement());
-            if (ent.indexOf("/") > 0) {
-                String fil = ent.substring(0, ent.indexOf("/"));
-                System.out.println(fil);
-                File local = new File(fil);
-                if (!local.exists())
-                    local.mkdirs();
-            }
-            if (ent.indexOf(".") > 0) {
-                int n = ent.length();
-                String fil1 = ent.substring(ent.lastIndexOf("/") + 1, n);
-                System.out.println(fil1);
-                extract(jarFile.getName(), ent);
-            }
-        }
-    }
-
-    public static String proc(Object obj) {
-        JarEntry entry = (JarEntry) obj;
-        String name = entry.getName();
-        System.out.println("\nEntry Name: " + name);
-        return (name);
-    }
-
-    public static void extract(String jarName, String entryName) throws IOException, ZipException {
-        JarFile jar = new JarFile(jarName);
-        System.out.println(jarName + " opened.");
+    public static void main(String[] args) {
+        String jarFilePath = "h08-ab12cdef-Max-Mustermann-submission.jar";
+        String destDirectory = "./";
+        String fileToEdit = "/h08/CalculatorTests.java";
+        String oldText = "private <T extends Throwable> void testException";
+        String newText = "private <T extends Object> void testException";
+        String newJarFilePath = "h08-ab12cdef-Max-Mustermann-submission_t.jar";
 
         try {
-            // Get the entry and its input stream.
-
-            JarEntry entry = jar.getJarEntry(entryName);
-
-            // If the entry is not null, extract it. Otherwise, print a
-            // message.
-
-            if (entry != null) {
-                // Get an input stream for the entry.
-
-                InputStream entryStream = jar.getInputStream(entry);
-
-                try {
-                    // Create the output file (clobbering the file if it exists).
-
-                    File yourFile = new File(entry.getName());
-                    if (!yourFile.isDirectory()) {
-                        var parentPath = yourFile.getParent();
-                        if (parentPath != null) {
-                            var parent = new File(yourFile.getParent());
-                            parent.mkdirs();
-                        }
-                    }
-                    FileOutputStream file = new FileOutputStream(entry.getName());
-
-                    try {
-                        // Allocate a buffer for reading the entry data.
-
-                        byte[] buffer = new byte[1024];
-                        int bytesRead;
-
-                        // Read the entry data and write it to the output file.
-
-                        while ((bytesRead = entryStream.read(buffer)) != -1) {
-                            file.write(buffer, 0, bytesRead);
-                        }
-
-                        System.out.println(entry.getName() + " extracted.");
-                    } finally {
-                        file.close();
-                    }
-                } finally {
-                    entryStream.close();
-                }
-            } else {
-                System.out.println(entryName + " not found.");
-            } // end if
-        } finally {
-            jar.close();
-            System.out.println(jarName + " closed.");
+            unpackJar(jarFilePath, destDirectory);
+            replaceTextInFile(destDirectory + fileToEdit, oldText, newText);
+            repackJar(destDirectory, newJarFilePath);
+            System.out.println("JAR file updated successfully!");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
+
+    private static void unpackJar(String jarFilePath, String destDirectory) throws IOException {
+        FileInputStream fis = new FileInputStream(jarFilePath);
+        JarInputStream jis = new JarInputStream(fis);
+        JarEntry entry = jis.getNextJarEntry();
+
+        while (entry != null) {
+            String fileName = entry.getName();
+            File file = new File(destDirectory + File.separator + fileName);
+
+            if (fileName.endsWith("/")) {
+                file.mkdirs();
+            } else {
+                File parent = file.getParentFile();
+                if (!parent.exists()) {
+                    parent.mkdirs();
+                }
+
+                FileOutputStream fos = new FileOutputStream(file);
+                byte[] buffer = new byte[4096];
+                int bytesRead = -1;
+                while ((bytesRead = jis.read(buffer)) != -1) {
+                    fos.write(buffer, 0, bytesRead);
+                }
+
+                fos.close();
+            }
+
+            entry = jis.getNextJarEntry();
+        }
+
+        jis.close();
+        fis.close();
+    }
+
+    private static void replaceTextInFile(String filePath, String oldText, String newText) throws IOException {
+        File file = new File(filePath);
+        String fileContent = readFile(file);
+        String newContent = fileContent.replaceAll(oldText, newText);
+        writeFile(file, newContent);
+    }
+
+    private static void repackJar(String sourceDirectory, String newJarFilePath) throws IOException {
+        FileOutputStream fos = new FileOutputStream(newJarFilePath);
+        JarOutputStream jos = new JarOutputStream(fos);
+        addToJar(sourceDirectory, jos);
+        jos.close();
+        fos.close();
+    }
+
+    private static void addToJar(String sourceDirectory, JarOutputStream jos) throws IOException {
+        File sourceFile = new File(sourceDirectory);
+        if (sourceFile.isDirectory()) {
+            String[] files = sourceFile.list();
+            for (int i = 0; i < files.length; i++) {
+                addToJar(sourceFile.getPath() + File.separator + files[i], jos);
+            }
+        } else {
+            FileInputStream fis = new FileInputStream(sourceFile);
+            jos.putNextEntry(new JarEntry(sourceFile.getPath()));
+            byte[] buffer = new byte[4096];
+            int bytesRead = -1;
+            while ((bytesRead = fis.read(buffer)) != -1) {
+                jos.write(buffer, 0, bytesRead);
+            }
+            jos.closeEntry();
+            fis.close();
+        }
+    }
+
+    private static String readFile(File file) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        StringBuilder stringBuilder = new StringBuilder();
+        String line = null;
+
+        while ((line = reader.readLine()) != null) {
+            stringBuilder.append(line).append("\n");
+        }
+
+        reader.close();
+        return stringBuilder.toString();
+    }
+
+    private static void writeFile(File file, String content) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+        writer.write(content);
+        writer.close();
     }
 }
